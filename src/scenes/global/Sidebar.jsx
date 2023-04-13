abc65f78acd837b73e9354cdffa87afc438f2114
akmaldira <akmaldiraa@gmail.com>
2023-04-14 05:01:40 +0700

    change app name

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -73,7 +73,7 @@ const Sidebar = () => {
                 ml="15px"
               >
                 <Typography variant="h3" color={colors.grey[100]}>
-                  ADMINIS
+                  WASTE BANK
                 </Typography>
                 <IconButton onClick={() => setIsCollapsed(!isCollapsed)}>
                   <MenuOutlinedIcon />
